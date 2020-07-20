58ae9d753631137d46205c2a3a0c06d7ffed395f
taotao <oncwnuKJY6G8EpTfb-s3tMZD7Ers@git.weixin.qq.com>
2020-07-20 16:48:29 +0800

    fix swagger

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,9 +1,16 @@
 package model
 
+import "blog_service/pkg/app"
+
 type Tag struct {
 	*Model
 	Name  string `json:"name"`
 	State uint8  `json:"state"`
+}
+
+type TagSwagger struct {
+	List  []*Tag
+	Pager *app.Pager
 }
 
 func (Tag) TableName() string {
